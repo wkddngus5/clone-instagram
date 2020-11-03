b25f24ac1e26e3e961a0c6470409c055cf09d992
wkddngus5 <wkddngus5@naver.com>
2020-11-03 15:41:08 +0900

    feat: Popover initialize

--- components/Header/Profile.js
+++ components/Header/Profile.js
@@ -1,4 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
+import Popover from '../Popover';
+import Divider from '../Divider';
 import styles from './Profile.module.css';
 
 function Profile({ viewer, className }) {
@@ -18,15 +20,29 @@ function Profile({ viewer, className }) {
         });
     }, []);
 
+    const popoverContent = (
+        <ul className={styles.profileMenuList}>
+            <li>프로필</li>
+            <li>저장됨</li>
+            <li>설정</li>
+            <li>계정전환</li>
+            <Divider />
+            <li>로그아웃</li>
+        </ul>
+    )
+
     return (
         <div
             ref={ref}
             className={`${className} ${styles.profile} ${isActive ? styles.isActive : ''}`}>
-            <span className={styles.profileSpan}>
-                <img
-                    src={profile || "/profile.png"}
-                    className={styles.profileImage} />
-            </span>
+            <Popover
+                content={popoverContent}>
+                <span className={styles.profileSpan}>
+                    <img
+                        src={profile || "/profile.png"}
+                        className={styles.profileImage} />
+                </span>
+            </Popover>
         </div>
     );
 }
---
+++ components/Popover/index.js
@@ -0,0 +1,77 @@
+import { useState, useEffect, useRef, useMemo } from 'react';
+import styles from './Popover.module.css';
+
+function Profile({ content, children }) {
+    const contentRef = useRef();
+    const childrenRef = useRef();
+    const [isActive, setIsActive] = useState(false);
+    const [popoverStyle, setPopoverStyle] = useState({
+        top: 0,
+        left: 0,
+    });
+    const [popoverArrowStyle, setPopoverArrowStyle] = useState({
+        top: 0,
+        left: 0,
+    });
+
+    useEffect(() => {
+        document.addEventListener('click', ({ target, clientX, clientY }) => {
+            const { current: childrenRefCurrent } = childrenRef;
+            const isOutSideClicked = !childrenRefCurrent.contains(target);
+            if (isOutSideClicked) {
+                setIsActive(false);
+                return;
+            }
+            const isActivePopover = childrenRefCurrent.classList.contains(styles.isActiveChildren);
+            console.log()
+            setIsActive(!isActivePopover);
+            const {
+                offsetTop,
+                offsetLeft,
+            } = childrenRefCurrent;
+            const {
+                width,
+                height,
+            } = childrenRefCurrent.getBoundingClientRect();
+
+            const {
+                width: contentWidth,
+                height: contentHeight,
+            } = contentRef.current.getBoundingClientRect();
+
+            setPopoverStyle({
+                top: `${offsetTop + height + 14}px`,
+                left: `${offsetLeft - contentWidth + 50}px`,
+                width: contentWidth,
+                height: contentHeight,
+            });
+            setPopoverArrowStyle({
+                left: `${contentWidth - 50 + (14 / 2)}px`,
+            })
+        }, []);
+    }, []);
+
+    return (
+        <>
+            <div
+                className={`${styles.Popover} ${isActive ? styles.isActivePopover : ''}`}
+                style={popoverStyle}>
+                <div
+                    className={styles.popoverArrow}
+                    style={popoverArrowStyle} />
+                <div
+                    ref={contentRef}
+                    className={styles.popoverContent}>
+                    {content}
+                </div>
+            </div>
+            <div
+                ref={childrenRef}
+                className={`${styles.popoverChildren} ${isActive ? styles.isActiveChildren : ''}`}>
+                {children}
+            </div>
+        </>
+    )
+}
+
+export default Profile;
