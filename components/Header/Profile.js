a1f80a3426964758d6a9da4052f6a4342934207e
wkddngus5 <wkddngus5@naver.com>
2020-11-05 23:59:34 +0900

    feat: add Icons

--- components/Header/Profile.js
+++ components/Header/Profile.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import Popover from '../Popover';
 import Divider from '../Divider';
+import { Profile as ProfileIcon, Bookmark as BookmarkIcon, Setting as SettingIcon } from '../Icon';
 import styles from './Profile.module.css';
 
 function Profile({ viewer, className }) {
@@ -8,14 +9,16 @@ function Profile({ viewer, className }) {
     const [isActive, setIsActive] = useState(false);
     const { profile } = viewer;
 
-    useEffect( () => {
-        document.addEventListener('click', ({target}) => {
+    useEffect(() => {
+        document.addEventListener('click', ({ target }) => {
             const isOutSideClicked = !ref.current.contains(target);
             if (isOutSideClicked) {
                 setIsActive(false);
                 return;
             }
-            const isActiveProfile = ref.current.classList.contains(styles.isActive);
+            const isActiveProfile = ref.current.classList.contains(
+                styles.isActive
+            );
             setIsActive(!isActiveProfile);
         });
     }, []);
@@ -24,44 +27,45 @@ function Profile({ viewer, className }) {
         <ul className={styles.profileMenuList}>
             <a>
                 <li className={styles.profileMenu}>
+                    <ProfileIcon />
                     프로필
                 </li>
             </a>
             <a>
                 <li className={styles.profileMenu}>
+                    <BookmarkIcon />
                     저장됨
                 </li>
             </a>
             <a>
                 <li className={styles.profileMenu}>
+                    <SettingIcon />
                     설정
                 </li>
             </a>
             <a>
-                <li className={styles.profileMenu}>
-                    계정전환
-                </li>
+                <li className={styles.profileMenu}>계정전환</li>
             </a>
-            <Divider
-                style={{margin: '0 0 15px 0' }} />
+            <Divider style={{ margin: '0 0 15px 0' }} />
             <a>
-                <li className={styles.profileMenu}>
-                    로그아웃
-                </li>
+                <li className={styles.profileMenu}>로그아웃</li>
             </a>
         </ul>
-    )
+    );
 
     return (
         <div
             ref={ref}
-            className={`${className} ${styles.profile} ${isActive ? styles.isActive : ''}`}>
-            <Popover
-                content={popoverContent}>
+            className={`${className} ${styles.profile} ${
+                isActive ? styles.isActive : ''
+            }`}
+        >
+            <Popover content={popoverContent}>
                 <span className={styles.profileSpan}>
                     <img
-                        src={profile || "/profile.png"}
-                        className={styles.profileImage} />
+                        src={profile || '/profile.png'}
+                        className={styles.profileImage}
+                    />
                 </span>
             </Popover>
         </div>
